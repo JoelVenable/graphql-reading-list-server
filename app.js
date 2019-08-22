fd3939b45e009869332c9b831b1ec7e17ec29368
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-21 19:05:25 -0500

    Passed schema to graphql service

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-
+const { Schema } = require('./schema/schema');
 
 const app = express();
 
 
 app.use('/graphql', graphqlHTTP({
-
+  Schema
 }));
 
 app.listen(4000, () => {
--- schema/schema.js
+++ schema/schema.js
@@ -1,2 +1,39 @@
-const graphql = require('graphql');
+const graphql, { GraphQLObjectType, GraphQLInteger, GraphQLString, GraphQLSchema } = require('graphql');
 
+const BookType = new GraphQLObjectType({
+  name: 'Book',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  })
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInteger },
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    book: {
+      type: BookType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        args.id
+        // get data from db
+      }
+    }
+  }
+});
+
+export const Schema = new GraphQLSchema({
+  query: RootQuery
+});
