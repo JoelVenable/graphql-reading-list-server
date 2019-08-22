bc6bec248dfb1e36dc45699a9cc028a69b69f824
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-22 08:11:56 -0500

    Added .env file and npm package

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { schema } = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+
+mongoose.connect('');
 
 
 app.use('/graphql', graphqlHTTP({
