e44baa6ea0843a1b597ea79b8440e26c3ddd8fde
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-08-22 21:08:10 -0500

    Added and used CORS package

--- app.js
+++ app.js
@@ -4,16 +4,21 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const { schema } = require('./schema/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+
 
 const app = express();
 
+app.use(cors);
+
 const db = {
   user: process.env.MONGO_DB_USER,
-  password: encodeURIComponent(process.env.MONGO_DB_PASSWORD),
+  password: process.env.MONGO_DB_PASSWORD,
   server: process.env.MONGO_DB_SERVER
 }
 
-mongoose.connect(encodeURI(`mongodb+srv://${db.user}:${db.password}@${db.server}`));
+mongoose.connect(encodeURI(`mongodb+srv://${db.user}:${db.password}@${db.server}`), { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
   console.log("Connected");
 });
